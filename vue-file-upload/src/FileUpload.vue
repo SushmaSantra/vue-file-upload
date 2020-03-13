2975dae032bf05d22da41d69bf4c66612ea74bc5
sushmasantra <sushmasantra@credenceanalytics.com>
2020-03-13 11:39:34 +0530

    Add component

---
+++ vue-file-upload/src/FileUpload.vue
@@ -0,0 +1,21 @@
+<script>
+import { setTimeout } from "timers";
+export default {
+  name: "FileUpload",
+  props: {
+    message: {
+      type: String,
+      default: "Hello Dev.to"
+    },
+    timeout: {
+      type: Number,
+      default: 0
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      alert(this.message);
+    }, this.timeout);
+  }
+};
+</script>
---
+++ vue-file-upload/src/index.js
@@ -0,0 +1,28 @@
+// ./src/index.js
+
+import component from "./fileUpload.vue";
+
+function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component("FileUpload", component);
+}
+
+const plugin = {
+  install
+};
+
+let GlobalVue = null;
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.vue;
+}
+
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+component.install = install;
+
+export default component;
